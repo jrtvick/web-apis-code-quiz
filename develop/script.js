e9d74a9435089fae9cc0c14f42a2a18cf4a43c09
Tyler Vick <jrtvick@gmail.com>
2023-05-07 15:35:32 -0400

    established html and began work on css and js

---
+++ develop/script.js
@@ -0,0 +1,13 @@
+// Establish global variables
+
+// Create local saves for highscore value and the player's initials
+
+// Create a timer function that launches upon start button being clicked
+
+// Create a function in which the clock decreases by a set amount of seconds if a wrong answer is selected
+
+// Create a current score tracker
+
+// Create the question+answers portion of the quiz being displayed as the user clicks through
+
+// Create an event listener for the start game button being pressed
