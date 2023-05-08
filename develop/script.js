4f586df2db973de09352a10ca5957e2ecee3f90d
Tyler Vick <jrtvick@gmail.com>
2023-05-08 16:09:10 -0400

    added in some rough javascript ideas

--- develop/script.js
+++ develop/script.js
@@ -3,20 +3,92 @@ var startButton = document.querySelector(".start-button");
 
 // Create a function for the game starting
 function startGame() {
-    
+
 }
 
 // Create a current score tracker
 
 // Create a timer function that launches upon start button being clicked
+// Create a function in which the clock decreases by a set amount of seconds if a wrong answer is selected
+// Create local saves for highscore value and the player's initials
+
+// function setTime() {
+//     // Sets interval in variable
+//     var timerInterval = setInterval(function() {
+//       secondsLeft--;
+//       timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+  
+//       if(secondsLeft === 0) {
+//         // Stops execution of action at set interval
+//         clearInterval(timerInterval);
+//         // Calls function to create and append image
+//         sendMessage();
+//       }
+  
+//     }, 1000);
+//   }
+
+
+
+
+//   var counter = document.querySelector("#counter");
+//   var addButton = document.querySelector("#add");
+//   var subtractButton = document.querySelector("#subtract");
+  
+//   var count = localStorage.getItem("count");
+  
+//   counter.textContent = count;
+  
+//   addButton.addEventListener("click", function() {
+//     if (count < 24) {
+//       count++;
+//       counter.textContent = count;
+//       localStorage.setItem("count", count);
+//     }
+//   });
+  
+//   subtractButton.addEventListener("click", function() {
+//     if (count > 0) {
+//       count--;
+//       counter.textContent = count;
+//       localStorage.setItem("count", count);
+//     }
+//   });
+
+
+
+//   saveButton.addEventListener("click", function(event) {
+//     event.preventDefault();
+
+//   var studentGrade = {
+//     student: student.value,
+//     grade: grade.value,
+//     comment: comment.value.trim()
+//   };
+  
+//   localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
+//   renderMessage();
+  
+//   });
+  
+//   function renderMessage() {
+//     var lastGrade = JSON.parse(localStorage.getItem("studentGrade"));
+//     if (lastGrade !== null) {
+//       document.querySelector(".message").textContent = lastGrade.student + 
+//       " received a/an " + lastGrade.grade
+//     }
+//   }
+
 
 // Create the question+answers portion of the quiz being displayed as the user clicks through
 
-// Create a function in which the clock decreases by a set amount of seconds if a wrong answer is selected
+
 
 // Create an alert for end of game + final score
 
-// Create local saves for highscore value and the player's initials
+if (timer === 0) {
+    alert("Game over. Final score:" + currentScore(""));
+} 
 
 // Create an event listener for the start game button being pressed
 startButton.addEventListener("click", startGame);
